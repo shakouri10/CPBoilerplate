9764cf37e12793229e3c569164896a127cbc6d4c
shakouri10 <ms.shakouri83@gmail.com>
2023-10-19 19:42:08 +0330

    finished step 4-1 and 4-2 have issue

--- 4-2_RemoveTheWrongImg.py
+++ 4-2_RemoveTheWrongImg.py
@@ -1,42 +1,49 @@
 import os
-import easyocr
 import json
+from PIL import Image
+from pytesseract import image_to_string
 
-cdn_directory = "cdn/"
-reader = easyocr.Reader(['fa'])  # Initialize with Farsi (fa) language
-
-# List of images to delete
-images_to_delete = []
-
-# Iterate over images in CDN
-for img_file in os.listdir(cdn_directory):
-    img_path = os.path.join(cdn_directory, img_file)
+def check_image_for_text(img_path):
+    """Check if the image contains text using pytesseract"""
     try:
-        result = reader.readtext(img_path)
-        # If text is detected, add to the deletion list
-        if result:
-            images_to_delete.append(img_file)
+        img = Image.open(img_path)
+        text = image_to_string(img, lang='eng')
+        return len(text.strip()) > 0
     except Exception as e:
         print(f"Error processing image {img_path}: {e}")
+        return False
 
-# Now, you can remove images from the CDN directory and from the JSON
-for img in images_to_delete:
-    os.remove(os.path.join(cdn_directory, img))
+# Define the CDN directory path
+cdn_directory = "cdn/"
 
-# Removing from JSON
-with open("ExtractedFixImg.json", "r", encoding="utf-8") as file:
-    data = json.load(file)
+# Load the JSON data
+with open("ExtractedFixImg_v1.json", "r", encoding="utf-8") as file:
+    extracted_data = json.load(file)
 
-for entry in data:
-    if entry.get("header_picture_url", "").split("/")[-1] in images_to_delete:
+# List to track images to remove from JSON
+images_to_remove = []
+
+# Check each image in the CDN directory
+for image_file in os.listdir(cdn_directory):
+    img_path = os.path.join(cdn_directory, image_file)
+    if check_image_for_text(img_path):
+        print(f"Removing image {img_path} as it contains text.")
+        os.remove(img_path)
+        images_to_remove.append(f"cdn/{image_file}")
+
+# Remove the corresponding entries from JSON
+for entry in extracted_data:
+    if entry.get("header_picture_url", "") in images_to_remove:
         entry["header_picture_url"] = ""
-
     for section in entry.get("body", []):
         for content in section.get("body", []):
-            if content.get("type") in ["figure", "image"]:
-                if content["content"].split("/")[-1] in images_to_delete:
-                    content["content"] = ""
+            if content.get("type") == "image" and content.get("content") in images_to_remove:
+                section["body"].remove(content)
+            if content.get("type") == "figure":
+                for x in content.get("body", []):
+                    if x["type"] == "image" and x["content"] in images_to_remove:
+                        content["body"].remove(x)
 
 # Save the updated JSON
-with open("ExtractedFixImg.json", "w", encoding="utf-8") as file:
-    json.dump(data, file, ensure_ascii=False, indent=4)
+with open("ExtractedFixImg_v2.json", "w", encoding="utf-8") as file:
+    json.dump(extracted_data, file, ensure_ascii=False, indent=4)
